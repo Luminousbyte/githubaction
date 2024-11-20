372c749fe97a93a4053ba90f3dc7ba99c420ea29
Luminousbyte <Pinaki.seal32@gmail.com>
2024-11-20 12:43:03 +0530

    Unit test case updated.

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operations import addition,subtraction
+
+def test_add():
+    assert addition(2,3)==5
+    assert addition(-1,1)==0
+    
+def test_subtraction():
+    assert subtraction(5,3)==2
+    assert subtraction(4,3)==1
+    assert subtraction(3,3)==0
+    assert subtraction(2,3)==-1
